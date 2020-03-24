205d42b3f0939fd40c114f5e3a1dda9da6cae761
Mathias Müller <mathias.mueller@uzh.ch>
2020-03-24 16:11:43 +0100

    Do not fail if virtualenv not in PATH

--- scripts/make_virtualenv.sh
+++ scripts/make_virtualenv.sh
@@ -10,7 +10,7 @@ mkdir -p $base/venvs
 
 # python3 needs to be installed on your system
 
-virtualenv -p python3 $base/venvs/torch3
+python3 -m virtualenv -p python3 $base/venvs/torch3
 
 echo "To activate your environment:"
 echo "    source $base/venvs/torch3/bin/activate"
