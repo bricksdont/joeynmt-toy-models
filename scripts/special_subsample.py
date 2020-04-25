823a02c09a99e40869da9662ec9663a9a8133d97
Mathias Müller <mathias.mueller@uzh.ch>
2020-04-25 15:32:49 +0200

    debug sampling: memory-efficient

--- scripts/special_subsample.py
+++ scripts/special_subsample.py
@@ -58,12 +58,14 @@ def main():
     random_indexes = random.sample(range(num_lines), args.size)
 
     with open(args.src_input, "r") as src_input_handle, open(args.src_output, "w") as src_output_handle:
-        for index, src_line in enumerate(src_input_handle):
+        src_lines = src_input_handle.readlines()
+        for index, src_line in enumerate(src_lines):
             if index in random_indexes:
                 src_output_handle.write(src_line)
 
     with open(args.trg_input, "r") as trg_input_handle, open(args.trg_output, "w") as trg_output_handle:
-        for index, trg_line in enumerate(trg_input_handle):
+        trg_lines = trg_input_handle.readlines()
+        for index, trg_line in enumerate(trg_lines):
             if index in random_indexes:
                 trg_output_handle.write(trg_line)
 
