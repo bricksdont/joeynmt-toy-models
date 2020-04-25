506b0c60d6947ff95baaba4d9042fd9611369686
Mathias Müller <mathias.mueller@uzh.ch>
2020-04-25 16:39:55 +0200

    upgrade joey

--- scripts/download_install_packages.sh
+++ scripts/download_install_packages.sh
@@ -12,7 +12,7 @@ echo "Make sure this script is executed AFTER you have activated a virtualenv"
 
 git clone https://github.com/bricksdont/joeynmt.git $tools/joeynmt
 
-(cd $tools/joeynmt && git checkout factors && pip install --upgrade .)
+(cd $tools/joeynmt && git checkout factors && git pull && pip install --upgrade .)
 
 # install Moses scripts for preprocessing
 
